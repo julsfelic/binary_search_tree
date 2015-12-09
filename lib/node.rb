7edc036cdbfb22fefd4fe3642247e18978723a67
Julian Feliciano <julsfelic@gmail.com>
2015-12-08 18:44:15 -0700

    Add left link and right link attributes to Node class

--- lib/node.rb
+++ lib/node.rb
@@ -1,9 +1,11 @@
 require 'pry'
 
 class Node
-  attr_reader :data
+  attr_reader :data, :left_link, :right_link
 
   def initialize
     @data = nil
+    @left_link  = nil
+    @right_link = nil
   end
 end
--- test/node_test.rb
+++ test/node_test.rb
@@ -14,4 +14,12 @@ class NodeTest < Minitest::Test
   def test_has_data_that_is_equal_to_nil_on_creation
     assert_nil @node.data
   end
+
+  def test_has_a_left_link_that_is_equal_to_nil_on_creation
+    assert_nil @node.left_link
+  end
+
+  def test_has_a_right_link_that_is_equal_to_nil_on_creation
+    assert_nil @node.right_link
+  end
 end
